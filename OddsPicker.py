2fc116fe3eec5e82cfdb3115e341741917e29595
crossz at home <drcross@outlook.com>
2016-10-24 01:24:42 +0800

    OddsPicker: gradient testing

--- OddsPicker.py
+++ OddsPicker.py
@@ -19,22 +19,32 @@ game_data = pickle.load(open('game_data.p', 'rb'))
 i = 14
 
 game_os = game_data[i].odds_sets
-
-game_os_score = numpy.rot90(game_os)
-game_os_live = game_os[game_os_score[0] == u'-',:]
+game_os_score = game_os[:,4]
+game_os_live = game_os[game_os_score == u'-',:]
 
 print(game_os_live)
 
 # %% pick the odds serie of fav
-o1 = numpy.rot90(game_os_live)
-
-odds_hda = o1[2:5,:]
-odds_ave = numpy.average(odds_hda, axis=1)
-
+odds_hda = game_os_live[:,0:3]
+odds_ave = numpy.average(odds_hda, axis=0)
 odds_min_index = numpy.argmin(odds_ave)
+odds_serie = odds_hda[:,odds_min_index]
 
-odds_serie = odds_hda[odds_min_index]
+print(odds_serie)
 
 # %%
-o2 = odds_serie
+p1 = 1/odds_serie
+g1 = numpy.gradient(p1)
+
+## test numpy.gradient
+for i in range(len(p1)):
+    if i == 0:
+        gt = p1[1] - p1[0]
+    elif i == len(p1)-1:
+        gt = p1[len(p1)-1] - p1[len(p1)-2]
+    else:    
+        gt = (p1[i+1] - p1[i-1])/2
+
+    print("pi={:.5f} -> g={:.5f} | gi={:.5f}".format(p1[i], gt, g1[i]))
+    
 
